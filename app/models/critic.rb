8f89204aa0583291eac6b9b78ac1add8972489fe
Paul Llanque <llanquepol@gmail.com>
2023-05-09 11:17:47 -0500

    ADD: validates to Critic model

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,3 +1,8 @@
 class Critic < ApplicationRecord
   belongs_to :game
+  belongs_to :user
+
+  # Validates
+  validates :title, :body, presence: { message: "can't be blank. Please write something!" }
+
 end
